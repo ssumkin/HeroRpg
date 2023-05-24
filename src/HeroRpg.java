b177c99ea34184ab953c0f111af5c580387abe1c
ssumkin <rms856@gmail.com>
2023-05-24 16:17:50 +0900

    map move update

--- src/HeroRpg.java
+++ src/HeroRpg.java
@@ -1,15 +1,14 @@
-import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
 public class HeroRpg {
-    int[] nowLocation(char[][] map) {
+    int[] heroLocation(char[][] map) {
         int[] index = new int[2];
 
         for(int i = 0; i < map.length; i++) {
             for(int j = 0; j < map[i].length; j++) {
-                if(map[i][j] == 1) {
+                if(map[i][j] == '▲') {
                     index[0] = i;
                     index[1] = j;
                 }
@@ -17,6 +16,22 @@ public class HeroRpg {
         }
         return index;
     }
+
+    int[][] mosterLocation(char[][] map) {
+        int[][] index = new int[5][2];
+
+        for(int i = 0; i < map.length; i++) {
+            for(int j = 0; j < map[i].length; j++) {
+                if(map[i][j] == '■') {
+                    index[i][0] = i;
+                    index[i][1] = j;
+                }
+            }
+        }
+
+        return index;
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         HeroRpg heroRpg = new HeroRpg();
@@ -28,13 +43,17 @@ public class HeroRpg {
         }
         map[0][0] = '▲';
 
-        int monster1 = rand.nextInt(5) + 1; 
+        int monster1;
+        
+        do {
+            monster1 = rand.nextInt(5);
+        } while(monster1 == 0);
+
         int monster2 = rand.nextInt(5); 
         int monster3 = rand.nextInt(5); 
         int monster4 = rand.nextInt(5); 
         int monster5 = rand.nextInt(5); 
 
-        
         map[0][monster1] = '■';
         map[1][monster2] = '■';
         map[2][monster3] = '■';
@@ -42,7 +61,9 @@ public class HeroRpg {
         map[4][monster5] = '■';
         
         int[] now;
-        int[] after = new int[2];
+        int[] after = new int[2]; 
+        int[][] monsterNow = heroRpg.mosterLocation(map);
+ 
 
         while(true) {
             for (char[] is1 : map) {
@@ -54,28 +75,67 @@ public class HeroRpg {
             System.out.print(">> ");
             int moveLocation = sc.nextInt();
 
-            now = heroRpg.nowLocation(map);
+            now = heroRpg.heroLocation(map);
             
             if(moveLocation == 6) {
-                map[now[0]][now[1]] = 0;
+
+                map[now[0]][now[1]] = '0';
                 after[0] = now[0];
                 after[1] = now[1]+1 < 5 ? now[1]+1 : now[1];
-                map[after[0]][after[1]] = 1;
+ 
+                if(monsterNow[after[0]][1] == after[1]) {
+                        after[1] = now[1];
+                }
+
+                map[after[0]][after[1]] = '▲';
+
             } else if(moveLocation == 4) {
-                map[now[0]][now[1]] = 0;
+
+                map[now[0]][now[1]] = '0';
                 after[0] = now[0];
                 after[1] = now[1]-1 > -1 ? now[1]-1 : now[1];
-                map[after[0]][after[1]] = 1;
+
+                if(monsterNow[after[0]][1] == after[1]) {
+                    after[1] = now[1];
+                }
+
+                map[after[0]][after[1]] = '▲';
             } else if(moveLocation == 8) {
-                map[now[0]][now[1]] = 0;
+
+                map[now[0]][now[1]] = '0';
                 after[0] = now[0]-1 > -1 ? now[0]-1 : now[0];
                 after[1] = now[1];
-                map[after[0]][after[1]] = 1;
+
+  
+                for(int i = 0; i < monsterNow.length; i++) {
+                    if(monsterNow[i][0] == after[0]) {
+                        if(monsterNow[i][1] == after[1]) {
+                            after[0] = now[0];
+                        }
+                    }
+                }
+
+                map[after[0]][after[1]] = '▲';
+
             } else if(moveLocation == 2) {
-                map[now[0]][now[1]] = 0;
+
+                map[now[0]][now[1]] = '0';
+                
                 after[0] = now[0]+1 < 5 ? now[0]+1 : now[0];
                 after[1] = now[1];
-                map[after[0]][after[1]] = 1;
+ 
+
+                for(int i = 0; i < monsterNow.length; i++) {
+                    if(monsterNow[i][0] == after[0]) {
+                        if(monsterNow[i][1] == after[1]) {
+                            after[0] = now[0];
+                        }
+                    }
+                }
+
+                  
+                map[after[0]][after[1]] = '▲';
+
             } else if(moveLocation == 0) {
                 break;
             } else {
